c7bbaf18483ebfe07ef151053fb769b6a372970f
Amisha <pamisha22@yahoo.com>
2020-06-08 14:36:23 -0400

    find & replace update

--- main.js
+++ main.js
@@ -3,21 +3,61 @@
 
 const findInput = document.querySelector(".find-input")
 const replaceInput = document.querySelector(".replace-input")
-const replaceAllButton = document.querySelector(".replace-all-button")
+let replaceAllButton = document.querySelector(".replace-all-button")
+//let rowvalue = document.querySelectorAll(".cell")
+//console.log(rowvalue)s
+
+replaceAllButton.addEventListener("click", function () {
+    let userInput = findInput.value
+    console.log(userInput)
+
+    for (let index = 0; index < rowElements.length; index++) {
+        let currentRowElement = rowElements[index]
+        let cellElements = getCellElements(currentRowElement)
+        console.log(cellElements)
+
+        let cellArray = document.getElementsByClassName("row", "cell")
+
+        for (let index = 0; index < cellArray.length; index++) {
+            vartomatch = console.log(cellArray[index])
+
+
+            if (vartomatch.includes(userInput)) {
+
+                console.log(vartomatch.includes(userInput))
+
+            }
+
+
+        }
+
+    }
+
+
+
+
+})
+
+
+
+
 
 // The following variable holds your OUTER ARRAY of row elements.
 // Later you will need an OUTER LOOP to loop over the individual elements within
 // this array.
-const rowElements = document.querySelectorAll(".row")
+let rowElements = document.querySelectorAll(".row")
 
 // When you call the function belwo, it will get and return an INNER ARRAY
 // containing the cell elements for a given row.
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
 // NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
-function getCellElements (currentRowElement) {
+function getCellElements(currentRowElement) {
+
     return currentRowElement.querySelectorAll(".cell")
+
 }
+
 
 
 // YOUR CODE GOES HERE
